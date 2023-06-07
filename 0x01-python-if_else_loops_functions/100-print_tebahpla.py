b1174ea7e2d85a14e52bd730a9752c23ea6a6354
yesewhareg <hanas5294@gmail.com>
2023-06-07 02:55:03 -0700

    Smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -4,5 +4,5 @@
 """Print the alphabet in reverse order alternating upper- and lower-case."""
 i = 0
 for c in range(ord('Z'), ord('a') - 1, -1):
-    print("{}".format(chr(c - i)), end=="")
+    print("{}".format(chr(c - i)), end="")
     i = 32 if i == 0 else 0
