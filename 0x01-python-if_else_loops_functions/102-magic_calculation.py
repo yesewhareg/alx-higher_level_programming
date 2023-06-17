7b4a7d4df2a256d330018f04fadbf66bd7b6d597
yesewhareg <hanas5294@gmail.com>
2023-06-17 00:17:31 -0700

    ByteCode -> Python #2

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 # 102-magic_calculation.py
 
-def magic_calculations(a, b, c):
-    """Match bytecode provvided by Holberton School."""
+
+def magic_calculation(a, b, c):
+    """Match bytecode provided by Holberton School."""
     if a < b:
         return (c)
     if c > b:
