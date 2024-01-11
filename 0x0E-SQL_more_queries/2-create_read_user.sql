9a82d4376d5d7368e52247a1aff14d3963820a4d
yesewhareg <hanas5294@gmail.com>
2024-01-11 04:18:25 -0800

    Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,6 @@
 -- creates the database hbtn_0d_2 and the user user_0d_2
--- creates a database CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- creates a database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- creates a user
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 -- grants SELECT privileges to a user
